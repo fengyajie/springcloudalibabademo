a8edee4c8091d38593f942bd46ceb148d798193f
fengyajie <fengyajie@fosunhealth.com>
2024-11-12 09:35:20 +0800

    feign

---
+++ api/src/main/java/com/common/api/RemoteBusinessApi.java
@@ -0,0 +1,4 @@
+package com.common.api;
+
+public interface RemoteBusinessApi {
+}
--- business-demo/src/main/java/com/business/businessdemo/BusinessDemoApplication.java
+++ business-demo/src/main/java/com/business/businessdemo/BusinessDemoApplication.java
@@ -3,9 +3,11 @@ package com.business.businessdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 //@EnableDiscoveryClient
+@EnableFeignClients
 @SpringBootApplication(scanBasePackages = {"com.business"})
 public class BusinessDemoApplication {
 
