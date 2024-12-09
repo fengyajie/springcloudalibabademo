70b184530d14433bf26288701c0a4306b1e7affe
fengyajie <fengyajie@fosunhealth.com>
2024-12-09 22:34:48 +0800

    异步消息

--- business-demo/src/main/java/com/business/businessdemo/service/BusnessMqSendService.java
+++ business-demo/src/main/java/com/business/businessdemo/service/BusnessMqSendService.java
@@ -1,11 +1,16 @@
 package com.business.businessdemo.service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.rocketmq.client.producer.SendCallback;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.client.producer.SendStatus;
 import org.apache.rocketmq.spring.core.RocketMQTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class BusnessMqSendService {
 
@@ -26,14 +31,31 @@ public class BusnessMqSendService {
         }*/
 
         //顺序发送
-        SendResult consumer = rocketMQTemplate.syncSendOrderly("business_topic:tag1", text1, "consumer");
+       /* SendResult consumer = rocketMQTemplate.syncSendOrderly("business_topic:tag1", text1, "consumer");
         SendResult consumer1 = rocketMQTemplate.syncSendOrderly("business_topic:tag1", "发送消息1", "consumer");
         SendResult consumer2 = rocketMQTemplate.syncSendOrderly("business_topic:tag1", "发送消息2", "consumer");
         SendResult consumer3 = rocketMQTemplate.syncSendOrderly("business_topic:tag1", "发送消息3", "consumer");
         SendResult consumer4 = rocketMQTemplate.syncSendOrderly("business_topic:tag1", "发送消息4", "consumer");
         if(!SendStatus.SEND_OK.equals(consumer.getSendStatus())){
             flag = false;
-        }
+        }*/
+
+
+        //延迟消息
+        //SendResult sendResult = rocketMQTemplate.syncSend("business_topic:tag1", MessageBuilder.withPayload("延迟消息").build(), 3000, 4);
+
+        rocketMQTemplate.asyncSend("business_topic:tag1", MessageBuilder.withPayload("异步消息").build(), new SendCallback() {
+            @Override
+            public void onSuccess(SendResult sendResult) {
+                log.info("异步消息发送成功");
+            }
+
+            @Override
+            public void onException(Throwable throwable) {
+                log.error("异步消息发送失败");
+            }
+        });
+
         return  flag;
 
     }
